9977c70290743940cb46bc028c3dd41d7ed8feae
nishant4138 <nishantavasthi7777@gmail.com>
2021-02-12 22:09:12 +0530

    second

---
+++ booking/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Booking } from './Booking';
+
+function App() {
+  return (
+    <div className="App">
+      <Booking/>
+    </div>
+  );
+}
+
+export default App;
--- shoppingapp/src/Onlineshopping.js
+++ shoppingapp/src/Onlineshopping.js
@@ -5,7 +5,8 @@ import Cart from './Cart';
  class Onlineshopping extends React.Component{
     render(){
         const Cartinfo=[{itemname:"latop", price: 3000},
-        {itemname:"lsdfsfatop", price: 893000},{itemname:"lasfsd", price: 45300}];
+        {itemname:"lsdfsfatop", price: 893000},
+        {itemname:"lasfsd", price: 45300}];
 
         return(
             <div classname ="mydiv">
